5bdc6e07ffffbfb5caf631509959e21de6d16b8e
Qiuyi Wang <wqysdmn0907@gmail.com>
2022-02-19 14:15:25 -0500

    Test version

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "https://annie-e-commerce.herokuapp.com/api/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+console.log(localStorage)
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -9,5 +10,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  // header: { token: `Bearer ${TOKEN}` },
 });
